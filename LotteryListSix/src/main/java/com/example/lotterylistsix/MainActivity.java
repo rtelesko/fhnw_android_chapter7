87855f0044033288888a3f35bee5e6901f21493e
rainer.telesko <rainer.telesko@fhnw.ch>
2022-04-04 10:00:23 +0200

    Remove unused import

--- LotteryListSix/src/main/java/com/example/lotterylistsix/MainActivity.java
+++ LotteryListSix/src/main/java/com/example/lotterylistsix/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.lotterylistsix;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
